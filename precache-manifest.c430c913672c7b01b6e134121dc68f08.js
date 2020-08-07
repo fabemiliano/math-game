faa9d4b5794394ac01d8f5f2a64f50f40dec6b82
fabemiliano <fab.emiliano@gmail.com>
2020-08-07 16:37:34 -0300

    Updates

---
+++ precache-manifest.c430c913672c7b01b6e134121dc68f08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34786949770f117176c0ce47c8cad66f",
+    "url": "/math-game/index.html"
+  },
+  {
+    "revision": "6763d3c1b7feb1be69aa",
+    "url": "/math-game/static/css/main.6b5d52ec.chunk.css"
+  },
+  {
+    "revision": "230686baac49b561ef18",
+    "url": "/math-game/static/js/2.1dd73a58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/math-game/static/js/2.1dd73a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6763d3c1b7feb1be69aa",
+    "url": "/math-game/static/js/main.d7674c6a.chunk.js"
+  },
+  {
+    "revision": "989f78b52cfc43888d36",
+    "url": "/math-game/static/js/runtime-main.aee31a93.js"
+  },
+  {
+    "revision": "c779c056b72036843cc113678c5e2b78",
+    "url": "/math-game/static/media/background.c779c056.jpg"
+  }
+]);
